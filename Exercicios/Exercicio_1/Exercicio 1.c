0bef678a6456bf510bfea4b5875ed763d962cac1
LucasSantos1551 <lucas.santos.t0126453@edu.atec.pt>
2025-02-20 10:43:45 +0000

    Exercicio 1

---
+++ Exercicios/Exercicio_1/Exercicio 1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int segundos;
+    int horas, minutos, segundos_restantes;
+
+    printf("Digite o número de segundos: ");
+    scanf("%d", &segundos);
+
+    horas = segundos / 3600;
+    minutos = (segundos % 3600) / 60;
+    segundos_restantes = segundos % 60;
+
+    printf("%d segundos equivalem a %d horas, %d minutos e %d segundos.\n", segundos, horas, minutos, segundos_restantes);
+
+    return 0;
+}
