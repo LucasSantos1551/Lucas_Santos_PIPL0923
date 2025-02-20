52346a893882897d06eb752ecf44f4baa538e96c
LucasSantos1551 <lucas.santos.t0126453@edu.atec.pt>
2025-02-20 10:51:22 +0000

    Exercicio 3

--- Exercicios/Exercicio_3/Exercicio3.c
+++ Exercicios/Exercicio_3/Exercicio3.c
@@ -2,22 +2,18 @@
 
 int main() {
     int num1, num2;
-
-    // Lê os dois números
     printf("Digite o primeiro número: ");
     scanf("%d", &num1);
     printf("Digite o segundo número: ");
     scanf("%d", &num2);
 
-    // Exibe em ordem crescente
+
     printf("Ordem crescente: ");
     if (num1 < num2) {
         printf("%d, %d\n", num1, num2);
     } else {
         printf("%d, %d\n", num2, num1);
     }
-
-    // Exibe em ordem decrescente
     printf("Ordem decrescente: ");
     if (num1 > num2) {
         printf("%d, %d\n", num1, num2);
