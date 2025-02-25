2c51222341e8d6416ce129e54a8c356e7fa4ea35
LucasSantos1551 <lucas.santos.t0126453@edu.atec.pt>
2025-02-25 14:58:36 +0000

    Exercicio 6

---
+++ Exercicios/Exercicio_6/Exercicio6.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    char nome[100];
+    float valorCompra, desconto, valorTotal;
+
+    printf("Digite o nome do cliente: ");
+    fgets(nome, sizeof(nome), stdin);
+
+    printf("Digite o valor da compra: ");
+    scanf("%f", &valorCompra);
+
+    if (valorCompra <= 200.00) {
+        desconto = 0.10;
+    } else if (valorCompra > 200.00 && valorCompra <= 500.00) {
+        desconto = 0.15;
+    } else {
+        desconto = 0.20;
+    }
+    float valorDesconto = valorCompra * desconto;
+    valorTotal = valorCompra - valorDesconto;
+
+    printf("\nNome do cliente: %s", nome);
+    printf("Valor da compra: %.2f€\n", valorCompra);
+    printf("Percentual de desconto: %.0f%%\n", desconto * 100);
+    printf("Valor do desconto: %.2f€\n", valorDesconto);
+    printf("Valor total a pagar: %.2f€\n", valorTotal);
+
+    return 0;
+}
