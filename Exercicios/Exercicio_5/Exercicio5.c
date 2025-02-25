ea856b2a28314904d635712c9f2f37a951db275d
LucasSantos1551 <lucas.santos.t0126453@edu.atec.pt>
2025-02-25 12:50:11 +0000

    Exercicio 5

---
+++ Exercicios/Exercicio_5/Exercicio5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int Num1, Num2, Num3;
+    int temp;
+
+
+    printf("Digite o primeiro valor: ");
+    scanf("%d", &Num1);
+    printf("Digite o segundo valor: ");
+    scanf("%d", &Num2);
+    printf("Digite o terceiro valor: ");
+    scanf("%d", &Num3);
+
+    if (Num1 > Num2) {
+        temp = Num1;
+        Num1 = Num2;
+        Num2 = temp;
+    }
+    if (Num1 > Num3) {
+        temp = Num1;
+        Num1 = Num3;
+        Num3 = temp;
+    }
+    if (Num2 > Num3) {
+        temp = Num2;
+        Num2 = Num3;
+        Num3 = temp;
+    }
+
+    printf("Ordem crescente: %d, %d, %d\n", Num1, Num2, Num3);
+
+    printf("Ordem decrescente: %d, %d, %d\n", Num3, Num2, Num1);
+
+    return 0;
+}
