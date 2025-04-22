988f9226a7cae743b1f45ca4ad938aa545bc1507
LucasSantos1551 <lucas.santos.t0126453@edu.atec.pt>
2025-04-22 12:31:18 +0100

    exercicios

---
+++ python/Ex1.py
@@ -0,0 +1,12 @@
+if num1 > num2 > num3:
+    print("num1 maior, num2 meio, num3 menor")
+elif num1 > num3 > num2:
+    print("num1 maior, num3 meio, num2 menor")
+elif num2 > num1 > num3:
+    print("num2 maior, num1 meio, num3 menor")
+elif num2 > num3 > num1:
+    print("num2 maior, num3 meio, num1 menor")
+elif num3 > num1 > num2:
+    print("num3 maior, num1 meio, num2 menor")
+else:
+    print("num3 maior, num2 meio, num1 menor")
---
+++ python/Untitled-1.py
@@ -0,0 +1,18 @@
+num = int(input("Digite um número de 1 a 13: "))
+
+if num == 1:
+    print("1 não é primo")
+elif num == 2:
+    print("2 é primo")
+elif num == 3:
+    print("3 é primo")
+elif num == 5:
+    print("5 é primo")
+elif num == 7:
+    print("7 é primo")
+elif num == 11:
+    print("11 é primo")
+elif num == 13:
+    print("13 é primo")
+else:
+    print(f"{num} não é primo")
