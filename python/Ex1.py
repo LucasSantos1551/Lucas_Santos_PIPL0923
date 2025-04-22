2ea581cf2fe89f30ce39787a945e2c446a46b554
LucasSantos1551 <lucas.santos.t0126453@edu.atec.pt>
2025-04-22 14:13:44 +0100

    #

--- python/Ex1.py
+++ python/Ex1.py
@@ -1,12 +1,9 @@
-if num1 > num2 > num3:
-    print("num1 maior, num2 meio, num3 menor")
-elif num1 > num3 > num2:
-    print("num1 maior, num3 meio, num2 menor")
-elif num2 > num1 > num3:
-    print("num2 maior, num1 meio, num3 menor")
-elif num2 > num3 > num1:
-    print("num2 maior, num3 meio, num1 menor")
-elif num3 > num1 > num2:
-    print("num3 maior, num1 meio, num2 menor")
-else:
-    print("num3 maior, num2 meio, num1 menor")
+i=1
+while i<=30:
+    if i % 2 == 0:
+        print(i,"par")
+    else:
+        print(i,"impar")
+    i+=1
+ 
+ 
